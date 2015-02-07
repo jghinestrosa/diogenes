40d812d8eb3e2cc283467fdd7467e00cc55692b9
jorge <jorgegarciahinestrosa@gmail.com>
2015-02-07 23:45:29 +0100

    Add spanish and english support for the menu

--- menu.js
+++ menu.js
@@ -1,12 +1,40 @@
-diogenes.Menu = function(actions) {
-  
-  this.actions = {};
+diogenes.Menu = function(language) {
 
-  var actionId = 1;
-  actions.forEach(function(action) {
-    var id = 'a' + actionId;
-    this.actions[id] = action;
-    actionId++;
-  }.bind(this));
+  language = language || diogenes.Menu.prototype.en;
+  this.actions = this[language];
 
+  //var actionId = 1;
+  //actions.forEach(function(action) {
+    //var id = 'a' + actionId;
+    //this.actions[id] = action;
+    //actionId++;
+  //}.bind(this));
+};
+
+diogenes.Menu.prototype = {};
+
+// Spanish language
+diogenes.Menu.prototype.es = {
+  give: 'Dar',
+  pickUp: 'Coger',
+  use: 'Usar',
+  open: 'Abrir',
+  talkTo: 'Hablar',
+  push: 'Empujar',
+  close: 'Cerrar',
+  lookAt: 'Mirar',
+  pull: 'Tirar'
+};
+
+// English language
+diogenes.Menu.prototype.en = {
+  give: 'Give',
+  pickUp: 'Pick up',
+  use: 'Use',
+  open: 'Open',
+  talkTo: 'Talk to',
+  push: 'Push',
+  close: 'Close',
+  lookAt: 'Look at',
+  pull: 'Pull'
 };
